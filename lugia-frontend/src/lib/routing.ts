0327ba2e01ea9b6e59d10831a92cb258e9f870ec
Robert <robert.bausmert@yahoo.de>
2025-06-02 11:38:25 +0900

    fix node_modules getting pushed to local from docker

--- lugia-frontend/src/lib/routing.ts
+++ lugia-frontend/src/lib/routing.ts
@@ -5,12 +5,12 @@ import { goto } from "$app/navigation";
  * This is a workaround for a bug in Svelte 5 where the active element is not blurred in time when navigating.
  * @param url - The URL to navigate to.
  */
-export async function safeGoto(url: string) {
+export async function safeGoto(url: string, options?: Parameters<typeof goto>[1]): Promise<void> {
 	// svelte 5s rendering bugs when using goto while an input is focused
 	const activeElement = document.activeElement as (Element & { blur?: () => void }) | null;
 	if (activeElement) {
 		activeElement.blur?.();
 	}
 
-	await goto(url);
+	await goto(url, options);
 }
