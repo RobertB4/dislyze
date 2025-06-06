1d4650fb615338b6e24e104931b5fba583b3af4e
Robert <robert.bausmert@yahoo.de>
2025-06-06 14:37:12 +0900

    fix format

--- lugia-frontend/src/routes/settings/profile/+page.ts
+++ lugia-frontend/src/routes/settings/profile/+page.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = () => {
-	// Profile page doesn't need additional data loading since 
+	// Profile page doesn't need additional data loading since
 	// user/tenant data is available via me object from layout
 	return {};
 };
