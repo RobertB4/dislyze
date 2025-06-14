c5d4bde3db1ad54268c9ee4234656c3588e828ba
Robert <robert.bausmert@yahoo.de>
2025-06-14 22:29:37 +0900

    trigger giratina ci

--- giratina-frontend/src/routes/+page.svelte
+++ giratina-frontend/src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import { Button, Input, Badge } from "@dislyze/zoroark";
+	import { Button, Input, Badge, Alert } from "@dislyze/zoroark";
 </script>
 
 <h1 class="px-4">Giratina Frontend</h1>
 <Button>testo2</Button>
 <Input id="asd" name="asd" label="ASD" />
 <Badge color="yellow">BADGE</Badge>
+<Alert>OH NO!</Alert>
